6602327048a178d1428db4c1281b926f6ce00493
josiah-williams <josiahdamiwilliams@gmail.com>
2026-03-07 17:24:33 +0100

    Add profile and application endpoints

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 // > **HireDeyGo** — _Find your next opportunity. Hire your next star._
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import profileRoutes from "./routes/profile.routes.js";
+import applicationRoutes from "./routes/application.routes.js";
+
+dotenv.config();
+
+const app = express();
+// connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API running...");
+});
+
+app.use("/api/profile", profileRoutes);
+app.use("/api/applications", applicationRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
