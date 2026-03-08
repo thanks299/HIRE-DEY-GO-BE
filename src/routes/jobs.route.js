73642424af97332ea4a8ec298665990fd2b1ad72
Ogbebor Aisosa Matthew <aisdevinstitute@gmail.com>
2026-03-08 23:04:08 +0100

    Implemented pagination, sorting and filtering logic, worked on update job, delete job and mark job as closed logic

--- src/routes/jobs.route.js
+++ src/routes/jobs.route.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { createJob, getJob, getJobs } from "../controllers/jobs.controller.js";
+import { closeJobPosting, createJob, deleteJob, getJob, getJobs, updateJob } from "../controllers/jobs.controller.js";
 
 const jobRoute = Router();
 
 jobRoute.get("/jobs", getJobs)
 jobRoute.get("/jobs/:id", getJob)
 jobRoute.post("/jobs", createJob)
-// jobRoute.put("/jobs/:id")
-// jobRoute.delete("/jobs/:id")
-// jobRoute.patch("/jobs/:id/close")
+jobRoute.put("/jobs/:id", updateJob)
+jobRoute.delete("/jobs/:id", deleteJob)
+jobRoute.patch("/jobs/:id/close", closeJobPosting)
 
 export default jobRoute
