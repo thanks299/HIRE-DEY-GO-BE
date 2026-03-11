05d559048e7f7351e946a9081da67088ed63e490
Aisdev <147014857+Aisdev979@users.noreply.github.com>
2026-03-11 06:47:57 +0100

    Cors (#7)
    * Created swagger-ui
    
    * trigger CI
    
    * trigger CI
    
    * Adding cors rules
    
    * Addedd cross-env
    
    * added cors import
    
    ---------
    
    Co-authored-by: josiah-williams <josiahdamiwilliams@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
-import express from "express"
+import express from "express";
 import errorMiddleware from "./middlewares/error.middleware.js"
-import authRoute from "./modules/auth/auth.routes.js"
-import jobRoute from "./modules/job/jobs.route.js"
+import authRoute from "./modules/auth/auth.routes.js";
+import jobRoute from "./modules/job/jobs.route.js";
 import profileRoutes from "./modules/profile/profile.routes.js";
 import applicationRoutes from "./modules/applications/application.routes.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger.js";
+import cors from "cors";
 
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+// Allowing all origin and allow credentials (like cookies or auth headers) too:
+app.use(cors({
+  origin: "*", // allow all origins
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true,
+}));
 
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 /**
  * @swagger
@@ -34,7 +41,7 @@ app.use("/api/v1/auth", authRoute);
 app.use("/api/v1", jobRoute);
 app.use("/api/v1", profileRoutes);
 app.use("/api/v1", applicationRoutes);
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 export default app
 
