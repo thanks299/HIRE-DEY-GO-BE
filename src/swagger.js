236ab73a535e956a7750956f9c59499181001b66
thanks299 <thanks299>
2026-03-13 15:43:17 +0100

    feat: add scoring endpoints with swagger documentation and integration tests

---
+++ src/swagger.js
@@ -0,0 +1,40 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Hire Dey Go API",
+      version: "1.0.0",
+      description: "API documentation for Hire Dey Go",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: [
+    "./src/modules/auth/*.js",
+    "./src/modules/job/*.js",
+    "./src/modules/scoring/*.js",
+    "./src/app.js",
+  ],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+export default swaggerSpec;
